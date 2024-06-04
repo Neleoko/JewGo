d73947761de38f7592c90e8d97219925722bc881
Neleoko <adda.ephraim@gmail.com>
2024-06-04 17:33:28 +0200

    Add component evenement.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {Evenement} from "../components/evenement";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up index.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function index() {
+    return (
+        <SafeAreaView className={"flex-1"}>
+            <ScrollView className={"flex-1"} style={{backgroundColor:"#F4F4F9"}}>
+                <View className={"flex-1 justify-center"}>
+                    <Evenement
+                        title={"Cours de Halakha Makhome"}
+                        image={require("../assets/images/fleg.jpg")}
+                        logoAsso={require("../assets/images/MakhomeLogo.jpg")}
+                        rabbin={"Rav Binyamin Smadja"}
+                        publicAgeMin={"18"}
+                        publicAgeMax={"35"}
+                        publicSexe={"Homme"}
+                    />
+                    <Evenement
+                        title={"Cours de Halakha Makhome"}
+                        image={require("../assets/images/chabath.jpg")}
+                        logoAsso={require("../assets/images/MakhomeLogo.jpg")}
+                        rabbin={"Rav Binyamin Smadja"}
+                        publicAgeMin={"18"}
+                        publicAgeMax={"35"}
+                        publicSexe={"Homme"}
+
+                    />
+                    <StatusBar style="auto" />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         marginHorizontal: 20,
+//         marginVertical: 20,
+//         backgroundColor: '#fff',
+//         justifyContent: 'center',
+//     },
+// });
