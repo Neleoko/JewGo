21914a23cf56bc232781b24c24a674ade3eb7468
Ephraim Adda <adda.ephraim@gmail.com>
2024-06-05 19:42:24 +0200

    Ajout de prop-types

--- src/app/(tabs)/eventView.tsx
+++ src/app/(tabs)/eventView.tsx
@@ -1,14 +1,16 @@
 import {View, Text, SafeAreaView, ScrollView} from "react-native";
 import React from "react";
+import { Entypo } from '@expo/vector-icons';
 
 
-export default function signUp() {
+export default function eventView() {
     return (
         <SafeAreaView className={"flex-1"}>
             <ScrollView className={"flex-1"} style={{backgroundColor:"#F4F4F9"}}>
                 <View className={"flex-1 justify-center m-5 border-2 rounded-lg bg-white p-3 flex-col"}>
-                    <View className={"flex-1"}>
-                    </View>
+                    <Entypo name="share" size={24} color="black" className={"items-end"} />
+                    <Text className={"text-2xl font-bold"}>Mardi 19 Mars</Text>
+                    <Text className={"text-xl"}>Cours de Halakha</Text>
                 </View>
             </ScrollView>
         </SafeAreaView>
