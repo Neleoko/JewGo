76950586290c05f23e1d805c8793b54b7c7e8e44
Ephraim Adda <adda.ephraim@gmail.com>
2024-06-19 22:57:01 +0200

    Ajout de la SliderBarAge.tsx dans les filtres

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import { View, TextInput, TouchableOpacity, Text, Modal } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import {ButtonCustom} from "./ButtonCustom";
-import SliderBar from './SliderBar';
-
+import SliderBarAge from './SliderBarAge';
+import { Dimensions } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 
 function Button(props: { onPress: () => void, title: string }) {
     return null;
@@ -13,6 +14,10 @@ export default function SearchBar() {
     const [searchQuery, setSearchQuery] = useState('');
     const [modalVisible, setModalVisible] = useState(false);
     const [valueSlider, setValueSlider] = useState(0);
+
+// Obtenez les dimensions de la fenêtre
+    const windowWidth = Dimensions.get('window').width;
+    const windowHeight = Dimensions.get('window').height;
 
 
     const handleSearch = (query) => {
@@ -26,6 +31,7 @@ export default function SearchBar() {
     };
 
     return (
+        <View style={{ position: 'absolute', top: 0, left: 0, right: 0, zIndex: 1 }}>
         <View className={"flex-row items-center my-2 p-3 bg-white rounded-lg shadow-md shadow-black border-2"} style={{borderColor: "#082385"}}>
             <TouchableOpacity className={"mr-2"}>
                 <Ionicons name="search" size={24} color="black" />
@@ -40,24 +46,33 @@ export default function SearchBar() {
                 <Ionicons name="filter" size={24} color="black" />
             </TouchableOpacity>
             <Modal
-                animationType="fade"
+                animationType="slide"
                 transparent={true}
                 visible={modalVisible}
                 onRequestClose={toggleModal}
             >
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-                    <View className={"bg-white p-4 border-2"}>
-                        <Text>Filtre 1</Text>
-                        <Text>Filtre 2</Text>
-                        <Text>Filtre 3</Text>
-
-                        {/*<View className={"flex-row"}>*/}
-                            {/*<ButtonCustom title="Fermer" handlePress={toggleModal}/>*/}
-                            {/*<ButtonCustom title="Appliquer" handlePress={toggleModal}/>*/}
-                        {/*</View>*/}
+                <View className={"bg-white p-7 flex-1"}>
+                    <View className={"flex-row justify-between"}>
+                        <Text className={"text-2xl font-bold"}>Filtres</Text>
+                        <TouchableOpacity onPress={toggleModal}>
+                            <Entypo name="cross" size={40} color="#082385" />
+                        </TouchableOpacity>
                     </View>
+                    <SliderBarAge onValuesChange={(values) => console.log(values)} />
+
+
+                    <Text>Filtre 1</Text>
+                    <Text>Filtre 2</Text>
+                    <Text>Filtre 3</Text>
+
+                    {/*<View className={"flex-row"}>*/}
+                        {/*<ButtonCustom title="Fermer" handlePress={toggleModal}/>*/}
+                        {/*<ButtonCustom title="Appliquer" handlePress={toggleModal}/>*/}
+                    {/*</View>*/}
                 </View>
+
             </Modal>
         </View>
+            </View>
     );
 }
